1421050385e1bc2684297f6e3da72a759fdf2823
Laith Bustani <139018824+hydrogennitrogen@users.noreply.github.com>
2023-11-23 09:53:53 -0500

    Code Corrections from CodeChecker

--- app/core/models.py
+++ app/core/models.py
@@ -13,7 +13,7 @@ class UserManager(BaseUserManager):
     """Manager for users."""
 
     def create_user(self, email, password=None, **extra_fields):
-        """Create, save, and return a new user."""
+        """Create, save and return a new user."""
         if not email:
             raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
@@ -23,7 +23,7 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password):
-        """Create and return a new superuser"""
+        """Create and return a new superuser."""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
